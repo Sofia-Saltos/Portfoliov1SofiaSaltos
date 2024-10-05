64e6b9b0d81b46a738c7c1b67869a3984b878d15
Sofia <s.sofiasaltos@gmail.com>
2024-10-05 13:43:47 -0700

    Commit1

---
+++ public/js/firebase.js
@@ -0,0 +1,24 @@
+/*
+  CCTB Website Development
+  IST105
+  Oct 2024
+  Description: This is a simple login website where students are asked to 
+  implement Social Network Login with Firebase
+*/
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDaw-H919JKZnxwF9SxKgIXKae9jI6sj5o",
+  authDomain: "portfoliov1sofiasaltos.firebaseapp.com",
+  projectId: "portfoliov1sofiasaltos",
+  storageBucket: "portfoliov1sofiasaltos.appspot.com",
+  messagingSenderId: "95971451605",
+  appId: "1:95971451605:web:290c3f1f0ab374f533f85c"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
